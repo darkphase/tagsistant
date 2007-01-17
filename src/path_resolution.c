1ab1ef8e63d964b7e0f3c68f041691f6a16a62b5
tx0 <tx0@a3defd00-1827-0410-870a-8e958c474122>
2007-01-17 11:04:54 +0000

    M    src/path_resolution.c M    src/mount.tagfs.c M    src/mount.tagfs.h

--- src/path_resolution.c
+++ src/path_resolution.c
@@ -1,3 +1,24 @@
+/*
+   TAGFS -- path_resolution.h
+   Copyright (C) 2006-2007 Tx0 <tx0@autistici.org>
+
+   Transform paths in queries and apply queries to file sets to
+   grep files matching with queries.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+*/
 #include "mount.tagfs.h"
 
 char *get_tag_name(const char *path)

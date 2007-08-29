850984cfc006223b6c3232d244308dcb5502e202
tx0 <tx0@a3defd00-1827-0410-870a-8e958c474122>
2007-08-29 09:26:26 +0000

    M    config.log M    src/Makefile M    config.status M    config.h M    Makefile

--- config.h
+++ config.h
@@ -154,13 +154,13 @@
 #define PACKAGE_NAME "tagsistant"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "tagsistant 0.0.20061220"
+#define PACKAGE_STRING "tagsistant 0.0.20070829"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "tagsistant"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.0.20061220"
+#define PACKAGE_VERSION "0.0.20070829"
 
 /* Define as the return type of signal handlers (`int' or `void'). */
 #define RETSIGTYPE void
@@ -169,7 +169,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.0.20061220"
+#define VERSION "0.0.20070829"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */

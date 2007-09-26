a145fdbfed3a21e3b402e4f056dd1388945881e7
tx0 <tx0@a3defd00-1827-0410-870a-8e958c474122>
2007-09-26 16:53:09 +0000

    M    tagsistant.c M    tagsistant.h M    Makefile.am A    tp_generic.c

---
+++ src/tp_generic.c
@@ -0,0 +1,48 @@
+/*
+   Tagsistant (tagfs) -- tp_generic.c
+   Copyright (C) 2006-2007 Tx0 <tx0@strumentiresistenti.org>
+
+   Tagsistant generic plugin which makes decisions on file MIME types.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+*/
+
+#include "tagsistant.h"
+
+/* declaring mime type */
+char mime_type[] = "*/*";
+
+/* exported init function */
+int plugin_init()
+{
+	dbg(LOG_INFO, "Plugin generic loaded, nice to meet you!");
+	fprintf(stderr, " Plugin generic loaded, nice to meet you!\n");
+	return 1;
+}
+
+/* exported processor function */
+int processor(const char *filename)
+{
+	return 2;
+}
+
+/* exported finalize function */
+void plugin_free()
+{
+	dbg(LOG_INFO, "Plugin generic gets unloaded, see you!");
+	fprintf(stderr, " Plugin generic gets unloaded, see you!\n");
+}
+
+// vim:ts=4:autoindent:nocindent:syntax=c

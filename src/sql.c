2c1937d4f45b58ca1c01b989962e5f1e32e87170
tx0 <tx0@a3defd00-1827-0410-870a-8e958c474122>
2009-07-06 23:53:12 +0000

    M    src/sql.c M    src/path_resolution.c M    src/tagsistant.c M    src/tagsistant.h

--- src/sql.c
+++ src/sql.c
@@ -111,3 +111,65 @@ int tagsistant_query(const char *format, int (*callback)(void *, int, char **, c
 
 	return res;
 }
+
+/***************\
+ * SQL QUERIES *
+\***************/
+#define CREATE_TAGS_TABLE		"create table tags(id integer primary key autoincrement not null, tagname varchar unique not null);"
+#define CREATE_TAGGED_TABLE		"create table tagged(id integer primary key autoincrement not null, tagname varchar not null, filename varchar not null);"
+#if TAGSISTANT_USE_CACHE_LAYER
+#	define CREATE_CACHE_TABLE	"create table cache_queries(id integer primary key autoincrement not null, path text not null, age datetime not null);"
+#	define CREATE_RESULT_TABLE	"create table cache_results(id integer not null, age datetime not null, filename varchar not null);"
+#endif
+#define CREATE_RELATIONS_TABLE	"create table relations(id integer primary key autoincrement not null, tag1 varchar not null, relation varchar not null, tag2 varchar not null);"
+
+void sql_create_tag(const char* tagname) {
+	tagsistant_query("insert into tags(tagname) values(\"%s\");", NULL, NULL, tagname);
+}
+
+void sql_delete_tag(const char *tagname) {
+	tagsistant_query("delete from tags where tagname = \"%s\"", NULL, NULL, tagname);
+	tagsistant_query("delete from tagged where tagname = \"%s\";", NULL, NULL, tagname);
+	tagsistant_query("delete from relations where tag1 = \"%s\" or tag2 = \"%s\";", NULL, NULL, tagname, tagname);
+#if TAGSISTANT_USE_CACHE_LAYER
+	tagsistant_query("delete from cache_queries where path like \"%%/%s/%%\" or path like \"%%/%s\";", NULL, NULL, tagname, tagname);
+#endif
+}
+
+void sql_tag_file(const char *tagname, const char *filename) {
+	tagsistant_query("insert into tagged(tagname, filename) values(\"%s\", \"%s\");", NULL, NULL, tagname, filename);
+}
+
+void sql_untag_file(const char *tagname, const char *filename) {
+	tagsistant_query("delete from tagged where tagname = \"%s\" and filename = \"%s\";", NULL, NULL, tagname, filename);
+}
+
+void sql_rename_tag(const char *tagname) {
+	tagsistant_query("update tags set tagname = \"%s\" where tagname = \"%s\";", NULL, NULL, tagname, tagname);
+	tagsistant_query("update tagged set tagname = \"%s\" where tagname = \"%s\";", NULL, NULL, tagname, tagname);
+}
+
+void sql_rename_file(const char *oldname, const char *newname) {
+	tagsistant_query("update tagged set filename = \"%s\" where filename = \"%s\";", NULL, NULL, newname, oldname);
+#if TAGSISTANT_USE_CACHE_LAYER
+	tagsistant_query("update cache_results set filename = \"%s\" where filename = \"%s\"", NULL, NULL, newname, oldname);
+#endif
+}
+
+#define ALL_FILES_TAGGED		"select filename from tagged where tagname = \"%s\""
+#define TAG_EXISTS				"select tagname from tags where tagname = \"%s\";"
+#define GET_ALL_TAGS			"select tagname from tags;"
+	
+#if TAGSISTANT_USE_CACHE_LAYER
+#	define IS_CACHED			"select id from cache_queries where path = \"%s\";"
+#	define ALL_FILES_IN_CACHE	"select filename from cache_results join cache_queries on cache_queries.id = cache_results.id where path = \"%s\";"
+#	define CLEAN_CACHE			"delete from cache_queries where age < datetime(\"now\"); delete from cache_results where age < datetime(\"now\");"
+#	define ADD_CACHE_ENTRY		"insert into cache_queries(path, age) values(\"%s\", datetime(\"now\", \"+15 minutes\"));"
+#	define ADD_RESULT_ENTRY		"insert into cache_results(id, filename, age) values(\"%lld\",\"%s\",datetime(\"now\", \"+15 minutes\"));"
+#	define GET_ID_OF_QUERY		"select id from cache_queries where path = \"%s\";"
+#	define GET_ID_OF_TAG		"select id from cache_queries where path like \"%%/%s/%%\" or path like \"%%/%s\";"
+#	define DROP_FILES			"delete from cache_results where id = %s;"
+#	define DROP_FILE			"delete from cache_results where filename = \"%s\" and id = %s;"
+#	define DROP_QUERY_BY_ID		"delete from cache_queries where id = %s;"
+#	define DROP_QUERY			"delete from cache_queries where path like \"%%/%s/%%\" or path like \"%%/%s\";"
+#endif

2a4f55ce3808aa5e7e78bc9d27f08a099916ad1d
tx0 <tx0@a3defd00-1827-0410-870a-8e958c474122>
2012-03-12 07:48:08 +0000

    Refactored some content from tagsistant.h into plugin.h and path_resolution.h
    M    src/sql.c
    M    src/utils.c
    A    src/plugin.h
    M    src/path_resolution.c
    M    src/tagsistant.c
    M    src/fuse_operations/symlink.c
    A    src/path_resolution.h
    M    src/tagsistant.h
    M    src/Makefile.am

---
+++ trunk/src/plugin.h
@@ -0,0 +1,62 @@
+/*
+   Tagsistant (tagfs) -- plugin.h
+   Copyright (C) 2006-2009 Tx0 <tx0@strumentiresistenti.org>
+
+   Tagsistant (tagfs) mount binary written using FUSE userspace library.
+   Header file
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+/* codes used in plugin chain processing */
+
+#define TP_ERROR	0	/**< an error occurred while processing with this plugin */
+#define TP_OK		1	/**< ok, but further tagging can be done by other plugins */
+#define TP_STOP		2	/**< this plugin is authoritative for mimetype, stop chaining */
+#define TP_NULL		3	/**< no tagging has been done, but that's not an error */
+
+/**
+ * holds a pointer to a processing function
+ * exported by a plugin
+ */
+typedef struct tagsistant_plugin {
+	/** MIME type managed by this plugins */
+	char *mime_type;
+
+	/** file name of this plugin */
+	char *filename;
+
+	/** handle to plugin returned by dlopen() */
+	void *handle;
+
+	/**
+	 * hook to processing function
+	 *
+	 * @param filename the file to be processed
+	 * @return 0 on failure (the plugin wasn't unable to process the file), 1 on
+	 *   partial success (the plugin did processed the file, but later processing
+	 *   by other plugins is allowed) or 2 on successful processing (no further
+	 *   processing required).
+	 */
+	int (*processor)(const tagsistant_querytree_t *qtree);
+
+	/**
+	 * hook to g_free allocated resources
+	 */
+	void (*free)();
+
+	/** next plugin in linked list */
+	struct tagsistant_plugin *next;
+} tagsistant_plugin_t;

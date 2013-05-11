b4895ea863870d73b80c0170b33edb151b2d3fbf
tx0 <tx0@a3defd00-1827-0410-870a-8e958c474122>
2013-05-11 16:43:59 +0000

    - Fixed a bug which prevented the creation of db= option in repository.ini   when default sqlite3 value was used. This also suppress the message:
        GLib-CRITICAL **: g_key_file_set_value: assertion `value != NULL' failed
    
    - Fixed a sever bug which prevented proper reasoning with SQLite3.
    
    M    src/utils.c
    M    src/buildnumber.h
    M    src/reasoner.c
    D    autom4te.cache
    M    debian/control

--- trunk/src/reasoner.c
+++ trunk/src/reasoner.c
@@ -147,15 +147,15 @@ int tagsistant_reasoner_inner(tagsistant_reasoning *reasoning, int do_caching)
 
 		// the result wasn't cached, so we lookup it in the DB
 		tagsistant_query(
-			"(select tags2.tagname from relations "
+			"select tags2.tagname from relations "
 				"join tags as tags1 on tags1.tag_id = relations.tag1_id "
 				"join tags as tags2 on tags2.tag_id = relations.tag2_id "
-				"where tags1.tagname = \"%s\") "
-			"union all"
-			"(select tags1.tagname from relations "
+				"where tags1.tagname = \"%s\" "
+			"union all "
+			"select tags1.tagname from relations "
 				"join tags as tags1 on tags1.tag_id = relations.tag1_id "
 				"join tags as tags2 on tags2.tag_id = relations.tag2_id "
-				"where tags2.tagname = \"%s\" and relation = \"is_equivalent\");",
+				"where tags2.tagname = \"%s\" and relation = \"is_equivalent\";",
 			reasoning->conn,
 			tagsistant_add_reasoned_tag_callback,
 			reasoning,
